76e0b59fafb0b9ca83022888bf40d80032cd2d85
Flint <Flintcheeze@yahoo.com>
2025-10-27 13:18:00 +0000

    refactor: expose create_app from package init

--- deepface_server/__init__.py
+++ deepface_server/__init__.py
@@ -1,1 +1,6 @@
-"""DeepFace Age Estimator package."""`n__version__ = "0.1.0"`n
+"""DeepFace Age Estimator package."""
+__version__ = "0.2.0"
+
+from .app_factory import create_app  # noqa: E402
+
+__all__ = ["create_app", "__version__"]
